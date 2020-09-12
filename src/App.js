bcfe1a04995fa103aa9c05d764721719df442296
Felisha Clay <34977224+fefeclay@users.noreply.github.com>
2020-09-12 07:21:21 -0500

    Finished Application

--- src/App.js
+++ src/App.js
@@ -29,27 +29,32 @@ class App extends Component {
     };
     const updatedItems = [...this.state.items, newItem];
 
-    this.setState(
-      {
-        items: updatedItems,
-        item: '',
-        id: uuidv4(),
-        editItem: false
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuidv4(),
+      editItem: false
+    });
   };
 
   clearList = () => {
-    console.log('clearList');
+    this.setState({ items: [] });
   };
 
   handleDelete = id => {
-    console.log(`handle delete ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({ items: filteredItems });
   };
 
   handleEdit = id => {
-    console.log(`handle edit ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true
+    });
   };
 
   render() {
